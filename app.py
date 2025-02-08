a3d12974fd169132fbfb6d872b8d1bae955a0307
Ali Korkmaz <alikorkmaz.fizik@gmail.com>
2025-02-08 19:09:06 +0300

    v2

--- app.py
+++ app.py
@@ -198,5 +198,7 @@ def download_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))  # Render provides a dynamic port
+    app.run(host="0.0.0.0", port=port, debug=False)
+
